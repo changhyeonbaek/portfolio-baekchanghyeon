3fa36ab0bfbe74980729f11e87da255744b779aa
Changhyeon Baek <sabondev@gmail.com>
2025-02-21 15:11:04 +0900

    메인 페이지 컴포넌트 생성

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import Skip from '../components/Skip'
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Intro from '../components/Intro'
+import Skill from '../components/Skill'
+import Port from '../components/Port'
+import Site from '../components/Site'
+import Contact from '../components/Contact'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Skip/>
+      <Header/>
+      <Main>
+            <Intro />
+            <Skill />
+            <Site />
+            <Port />
+            <Contact />
+        </Main>
+    </>
   )
 }
 
