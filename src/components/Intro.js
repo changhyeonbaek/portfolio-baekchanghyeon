fc5200a09ee44098088fa5cb6208beed3247d6cc
Changhyeon Baek <sabondev@gmail.com>
2025-02-21 16:29:11 +0900

    인트로 페이지 컴포넌트 생성

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,43 @@
 import React from 'react'
 
+import about from '../assets/img/img-user-05.jpg'
+import introContent from '../assets/data/intro-content.json'
+
 const Intro = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="intro">
+      <div className="intro-inner">
+        <h1 className="intro-title">{introContent.title}</h1>
+        <div class="intro-lines" aria-hidden="true">
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+        </div>
+      </div>
+      <div class="intro-text">
+        <div class="text">
+          <div>{introContent.desc[0]}</div>
+          <div>{introContent.desc[1]}</div>
+          <div>{introContent.desc[2]}</div>
+        </div>
+        <div class="img">
+          <img src={about} alt="어바웃" />
+        </div>
+      </div>
+      <div class="intro-lines bottom" aria-hidden="true">
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+      </div>
+    </section>
   )
 }
 
