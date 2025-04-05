5cb8ffc0287c17dcfb5910eab1e6a5168c43fc20
Changhyeon Baek <sabondev@gmail.com>
2025-04-05 14:30:49 +0900

    Remove unused components from Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,13 @@
 import React from 'react'
-import Skip from '../components/Skip'
 import Header from '../components/Header'
-import Main from '../components/Main'
 import Intro from '../components/Intro'
-import Skill from '../components/Skill'
-import Port from '../components/Port'
-import Site from '../components/Site'
-import Contact from '../components/Contact'
 
 const Home = () => {
   return (
-    <>
-      <Skip/>
-      <Header/>
-      <Main>
-            <Intro />
-            <Skill />
-            <Site />
-            <Port />
-            <Contact />
-        </Main>
-    </>
+    <div className="home-container">
+      <Header />
+      <Intro />
+    </div>
   )
 }
 
