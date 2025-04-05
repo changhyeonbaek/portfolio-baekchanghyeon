48def9f35035e08a76fff301fde64ae430d5bb4e
Changhyeon Baek <sabondev@gmail.com>
2025-04-05 14:25:33 +0900

    Update Header component with navigation link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,15 @@
-import React, { useState } from 'react'
-import headerNav from '../assets/data/header-nav.json'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
-  const [show, setShow] = useState(false)
-
-  const toggleMenu = () => {
-    setShow((prevShow) => !prevShow)
-  }
-
   return (
     <header className="header" role="banner">
       <div className="header-inner">
         <div className="header-logo">
-          <a href="/">
-            BAEKCHANGHYEON'S<em>Portfolio</em>
-          </a>
-        </div>
-        <nav
-          className={`header-nav ${show ? 'show' : ''}`}
-          role="navigation"
-          aria-label="메인 메뉴"
-        >
-          <ul>
-            {headerNav.map((nav, key) => (
-              <li key={key}>
-                <a href={nav.url}>{nav.title}</a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div
-          className="header-nav-mobile"
-          id="headerToggle"
-          aria-controls="primary-menu"
-          aria-expanded={show ? 'true' : 'false'}
-          role="button"
-          tabIndex="0"
-          onClick={toggleMenu}
-        >
-          <span></span>
+          <NavLink to="/">
+            <p>BAEKCHANGHYEON</p>
+          </NavLink>
+          <p>FRONTEND DEVELOPER</p>
         </div>
       </div>
     </header>
