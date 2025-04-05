f1a2fe775ff78523e25c7d8cf3a3524f43e4f01f
Changhyeon Baek <sabondev@gmail.com>
2025-04-05 14:49:13 +0900

    Update App.js component with React Router setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}/>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
